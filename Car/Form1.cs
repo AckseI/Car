3169eb336cba8b68866fbd1338db9381a4273e09
AckseI <36826218+AckseI@users.noreply.github.com>
2023-03-29 11:08:40 +0300

    Update Form1.cs

--- Car/Form1.cs
+++ Car/Form1.cs
@@ -1,4 +1,3 @@
-using System.Data;
 using System.Windows.Forms;
 
 namespace Car
@@ -39,6 +38,7 @@ namespace Car
             else {e.Handled = true;}
         }
 
+
         private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (char.IsDigit(e.KeyChar) | e.KeyChar == '\b' | e.KeyChar == Convert.ToChar(",")) return;
@@ -56,7 +56,6 @@ namespace Car
                 cars[i].sits = int.Parse(dataGridView1.Rows[i].Cells["sits"].Value.ToString());
             }
         }
-
         private void button3_Click(object sender, EventArgs e)
         {
             for (int i = 0; i < dataGridView1.RowCount; i++)
@@ -66,6 +65,53 @@ namespace Car
                 dataGridView1.Rows[i].Cells["isDamage"].Value = cars[i].isDamage;
                 dataGridView1.Rows[i].Cells["color"].Value = cars[i].color;
                 dataGridView1.Rows[i].Cells["sits"].Value = cars[i].sits;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bool isSort = false;
+
+            DateTime startTime = DateTime.Now;
+
+            while (!isSort)
+            {
+                isSort = true;
+                for (int i = 0; i < cars.Length - 1; i++)
+                {
+                    if (cars[i].maxSpeed > cars[i + 1].maxSpeed)
+                    {
+                        Car c = new Car();
+                        c = cars[i];
+                        cars[i] = cars[i+1];
+                        cars[i+1] = c; isSort = false;
+                    }
+                }
+            }
+            System.TimeSpan time = startTime - DateTime.Now;
+
+            label6.Text = time.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 10000; i++)
+            {
+                dataGridView1.Rows.Add(rnd.Next(300), 1, true, "jj", 5);
+            }
+            Array.Resize(ref cars, 0);
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                Array.Resize(ref cars, cars.Length+1);
+                cars[i] = new Car();
+                cars[i].maxSpeed = int.Parse(dataGridView1.Rows[i].Cells["maxSpeed"].Value.ToString());
+                cars[i].weight = float.Parse(dataGridView1.Rows[i].Cells["weight"].Value.ToString());
+                cars[i].isDamage = bool.Parse(dataGridView1.Rows[i].Cells["isDamage"].Value.ToString());
+                cars[i].color = dataGridView1.Rows[i].Cells["color"].Value.ToString();
+                cars[i].sits = int.Parse(dataGridView1.Rows[i].Cells["sits"].Value.ToString());
+
+
             }
         }
     }
